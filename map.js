962669513241f3fb839358f1d015ba333b8dc9ee
E.Azer Koçulu <azer@kodfabrik.com>
2013-04-28 02:42:05 -0700

    parallel

--- map.js
+++ map.js
@@ -1,44 +1,32 @@
 module.exports = map;
 
-/**
- * Apply async function to every item of iterable, receiving a callback function which takes error (if there is) and replacement parameters.
- *
- * @param {Function} fn
- * @param {Array, Object} iterable
- * @param {Function} callback
- */
-function map(fn, iterable, callback){
-  var clone, iter, i, len, key, value,
-      list = isArray(iterable);
+function newCallback(id, mem, array, results, callback){
+  return function(error, result){
 
-  iter = list ? Array.prototype.slice.call(iterable) : objectKeys(iterable);
-  clone = list ? iter : {};
+    if(mem.ret || ~mem.indexOf(id)) return;
 
-  (function next(i, error, rpl){
-
-    key = list ? i-1 : iter[i-1];
-    value = list ? clone[i] : iterable[ iter[i] ];
-
-    arguments.length > 2 && ( clone[key] = rpl );
-
-    if(error || i>=iter.length){
-      callback(error, clone);
+    if(error) {
+      mem.ret = true;
+      callback(error, results);
       return;
     }
 
-    fn(value, next.bind(undefined, i+1));
+    results[id] = result;
+    mem.push(id);
 
-  })(0);
-};
+    if(mem.length == array.length) callback(undefined, results);
 
-function objectKeys(obj){
-  var keys = [], key;
-  for(key in obj){
-    keys.push( key );
-  }
-  return keys;
+  };
 }
 
-function isArray(obj){
-  return Object.prototype.toString.call(obj) == '[object Array]';
+function map(fn, array, callback){
+
+  var result = [],
+      mem    = [],
+      i      = array.length;
+
+  while( i-- ) {
+    fn(array[i], newCallback(i, mem, array, result, callback));
+  }
+
 }
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var map = require('./map'),
+var map    = require('./map'),
     assert = require('assert');
 
 it('iterates arrays', function(done){
@@ -26,43 +26,13 @@ it('iterates arrays', function(done){
 
 });
 
-it('iterates objects', function(done){
-  var range = { 'a': 3, 'b': 1, 'c': 4 };
-
-  function fn(el,callback){ callback(undefined, el*el); }
-
-  map(fn, range, function(error, seq){
-
-    if(error){
-      throw error;
-    }
-
-    assert.equal(range.a, 3);
-    assert.equal(range.b, 1);
-    assert.equal(range.c, 4);
-
-    assert.equal(seq.a, 9);
-    assert.equal(seq.b, 1);
-    assert.equal(seq.c, 16);
-
-    done();
-
-  });
-
-});
-
 it('stops iteration when an error is returned', function(done){
   function fn(_,callback){
     callback(new Error('foobar'),9);
   }
 
   map(fn, [3, 1, 4], function(error, list){
-    assert.equal(list[0], 9);
-    assert.equal(list[1], 1);
-    assert.equal(list[2], 4);
-
-    assert.equal(error.message, 'foobar');
+    assert.deepEqual(list, []);
     done();
-
   });
 });
